80a682c2de590b988afe9da272e9e2406d64da3c
wanglei <wanglei@yiji.com>
2018-09-17 19:04:44 +0800

    no message

--- csgg/csgg_ui/src/main/java/com/csgg/appconfig/MainConfiguration.java
+++ csgg/csgg_ui/src/main/java/com/csgg/appconfig/MainConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("classpath*:spring/*.xml")
+@ImportResource("classpath*:spring/**/*.xml")
 public class MainConfiguration {
 
 	@Bean
