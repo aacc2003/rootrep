a0f3954bf98a55f9edf12a9ddf73012add3e614f
wanglei <wanglei@yiji.com>
2018-09-23 13:09:57 +0800

    no message

--- csgg/csgg_test/src/test/java/com/csgg/test/TestServerTest.java
+++ csgg/csgg_test/src/test/java/com/csgg/test/TestServerTest.java
@@ -3,6 +3,7 @@ package com.csgg.test;
 
 import javax.annotation.Resource;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.csgg.service.api.TestServer;
 import com.csgg.test.base.TestBase;
 
@@ -14,6 +15,7 @@ public class TestServerTest extends TestBase {
 //public class TestServerTest {
 
 	@Resource(name="testServerCxf")
+	@Reference
 	private TestServer	testR;
 	
 	public void testTestS() {
