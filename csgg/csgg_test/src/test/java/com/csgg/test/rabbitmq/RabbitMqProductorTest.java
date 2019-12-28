f8dd35d9077624dfe99545333c6c8c4294ddfde8
124991058@qq.com <124991058@qq.com>
2019-12-28 08:51:42 +0800

    mq1

---
+++ csgg/csgg_test/src/test/java/com/csgg/test/rabbitmq/RabbitMqProductorTest.java
@@ -0,0 +1,27 @@
+package com.csgg.test.rabbitmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class RabbitMqProductorTest {
+	static String exchange = "source";
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory cf = new ConnectionFactory();
+		cf.setAutomaticRecoveryEnabled(false);
+		
+		Connection cn = cf.newConnection();
+		
+		Channel cnl = cn.createChannel();
+		
+		String message = "xxxxx";
+		cnl.exchangeDeclare(exchange, "fanout");
+		cnl.basicPublish(exchange, "", null, message.getBytes());
+		
+		cnl.close();
+		cn.close();
+	}
+}
